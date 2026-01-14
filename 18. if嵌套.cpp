2c5c673e03fffb2e4dd350b5f6b55b180def1f2f
枫叶c <2744235537@qq.com>
2026-01-14 23:58:26 +0800

    语法

---
+++ 18. if嵌套.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    cin>>n;
+    if(n<0)cout<<"Negative Number";
+    else
+    {
+        if(n%2==1) cout<<"Odd Number";
+        else cout<<"Even Number";
+    }
+    
+    return 0;
+}
