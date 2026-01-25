3ae2283873c521b80b9616304f31747d4d722122
枫叶c <233671436+StellarVibes@users.noreply.github.com>
2026-01-25 20:24:57 +0800

    语言

---
+++ 16. 数学函数系列II.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>  
+using namespace std;
+
+int main()
+{
+    float d = 0;
+    cin >> d;
+    
+    printf("%.2f %.2f", pow(d,0.5),log(d));
+    
+    return 0;
+}
